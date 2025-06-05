fc36598998e2577089f3a831fe5997e350210332
Lorenzo Mantero <98963718+lollomante@users.noreply.github.com>
2025-06-05 12:07:20 +0200

    Update config.php

--- process/config.php
+++ process/config.php
@@ -1,15 +1,10 @@
 <?php 
 //connection to database
     define('SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
+    define('DB_USERNAME', '');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'gamelib');
-/*
-    define('SERVER', 'localhost');
-    define('DB_USERNAME', 'S4956547');
-    define('DB_PASSWORD', '7hnC6FSfdd');
-    define('DB_NAME', 'S4956547');
-*/
+
 //login
     define('REMEMBER_ME_DURATION', 1296000); // 15 giorni
 
